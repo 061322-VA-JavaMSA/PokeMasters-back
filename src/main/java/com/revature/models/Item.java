457e3e645b182e06414dfb3d2b3156a95bff23d9
Adam Harbeck <AHarbeck@student.fullsail.edu>
2022-07-19 14:18:30 -0700

    Item model, repo, and service

--- src/main/java/com/revature/models/Item.java
+++ src/main/java/com/revature/models/Item.java
@@ -1,5 +1,99 @@
 package com.revature.models;
 
-public class Item {
+import java.util.Objects;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import javax.persistence.Table;
+
+@Entity
+@Table(name="items")
+public class Item {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	@Column(name="api_id", nullable=false)
+	private int apiId;
+	@Column(name="price", nullable=false)
+	private int price;
+	@Column(name="effect", nullable= false)
+	private String effect;
+	@Column(name="type", nullable=false)
+	private String type;
+	
+	public Item() {
+		super();
+
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getApiId() {
+		return apiId;
+	}
+
+	public void setApiId(int apiId) {
+		this.apiId = apiId;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public String getEffect() {
+		return effect;
+	}
+
+	public void setEffect(String effect) {
+		this.effect = effect;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(apiId, effect, id, price, type);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Item other = (Item) obj;
+		return apiId == other.apiId && Objects.equals(effect, other.effect) && id == other.id && price == other.price
+				&& Objects.equals(type, other.type);
+	}
+
+	@Override
+	public String toString() {
+		return "Item [id=" + id + ", apiId=" + apiId + ", price=" + price + ", effect=" + effect + ", type=" + type
+				+ "]";
+	}
+	
+	
+	
 }
---
+++ src/main/java/com/revature/services/ItemService.java
@@ -0,0 +1,45 @@
+package com.revature.services;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.revature.exceptions.ItemNotFoundException;
+import com.revature.models.Item;
+import com.revature.repositories.ItemRepository;
+
+
+
+@Service
+public class ItemService {
+
+	private ItemRepository ir;
+
+	public ItemService(ItemRepository ir) {
+		super();
+		this.ir = ir;
+	}
+	
+	
+	public List<Item> getItems() {
+		return ir.findAll();
+	}
+	
+	public Item getItemById(int id) throws ItemNotFoundException{
+		Item item = ir.findById(id).orElseThrow(() -> new ItemNotFoundException());
+		return item;
+	}
+	
+	public Item addItem(Item item) {
+		return ir.save(item);
+	}
+	
+	public Item updateItem(Item item) {
+		return ir.save(item);
+	}
+	
+	public void deleteItem(int id) {
+		ir.deleteById(id);
+	}
+	
+}
