7f771aaaedefedaab490c4bc949dc8ed04baa207
Kevin Geronimo <96751432+kevingeronimo@users.noreply.github.com>
2022-07-21 15:54:06 -0400

    chore: add cors headers

--- src/main/java/com/revature/controllers/PokemonController.java
+++ src/main/java/com/revature/controllers/PokemonController.java
@@ -1,23 +1,14 @@
 package com.revature.controllers;
 
-import java.util.List;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.revature.exceptions.PokemonNotFoundException;
 import com.revature.models.Pokemon;
 import com.revature.services.PokemonService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "http://localhost:4200", allowCredentials = "true")
+import java.util.List;
+
 @RestController
 @RequestMapping("/pokemon")
 public class PokemonController {
