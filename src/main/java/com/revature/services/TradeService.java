55806b4a7325703b80d91bbd39ee5c4d7dab2bb8
Calvin Post <calvinhenry826@revature.net>
2022-07-29 17:08:52 -0400

    Working on back-end logic for trade feature

--- src/main/java/com/revature/services/TradeService.java
+++ src/main/java/com/revature/services/TradeService.java
@@ -2,11 +2,14 @@ package com.revature.services;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import com.revature.exceptions.TradeNotFoundException;
 import com.revature.models.Trade;
 import com.revature.models.Trainer;
 import com.revature.repositories.TradeRepository;
 
+@Service
 public class TradeService {
 
 	private TradeRepository tr;
@@ -24,7 +27,16 @@ public class TradeService {
 		return tr.findById(id).orElseThrow(() -> new TradeNotFoundException());
 	}
 	
+	public Trade createTrade(Trade t) {
+		t.setId(-1);
+		return tr.save(t);
+	}
+	
 	public Trade saveTrade(Trade t) {
 		return tr.save(t);
+	}
+	
+	public void deleteTrade(Trade t) {
+		tr.delete(t);
 	}
 }
