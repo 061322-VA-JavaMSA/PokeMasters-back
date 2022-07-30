1976de8ec3bbb3ce0fa0234cb91d82d655f5a665
Kevin Geronimo <96751432+kevingeronimo@users.noreply.github.com>
2022-07-30 03:51:53 -0400

    chore: docker image setup

--- src/main/java/com/revature/PokeMastersBackApplication.java
+++ src/main/java/com/revature/PokeMastersBackApplication.java
@@ -1,11 +1,15 @@
 package com.revature;
 
 import java.util.ArrayList;
+import java.util.List;
 
+import com.revature.util.HeaderRequestInterceptor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -52,7 +56,13 @@ public class PokeMastersBackApplication {
 	
 	@Bean
     public RestTemplate getRestTemplate() {
-        return new RestTemplate();
+		List<ClientHttpRequestInterceptor> interceptors = new ArrayList<>();
+		interceptors.add(new HeaderRequestInterceptor("Accept", MediaType.APPLICATION_JSON_VALUE));
+		interceptors.add(new HeaderRequestInterceptor("user-agent", "Application"));
+
+		RestTemplate restTemplate = new RestTemplate();
+		restTemplate.setInterceptors(interceptors);
+        return restTemplate;
     }
 
 	@Bean
