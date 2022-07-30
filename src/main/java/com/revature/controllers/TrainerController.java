75991e01e51235fe083633770ee86a0024bb5feb
Adam Harbeck <AHarbeck@student.fullsail.edu>
2022-07-30 14:18:09 -0700

    Implemented buying and retrieving of trainerItems

--- src/main/java/com/revature/controllers/TrainerController.java
+++ src/main/java/com/revature/controllers/TrainerController.java
@@ -1,18 +1,21 @@
 package com.revature.controllers;
 
-import com.revature.models.Trainer;
-import com.revature.services.TrainerService;
+import java.net.URI;
+import java.util.List;
+
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.net.URI;
-import java.util.List;
+import com.revature.models.Trainer;
+import com.revature.services.TrainerService;
 
 @RestController
 public class TrainerController {
@@ -38,5 +41,21 @@ public class TrainerController {
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Something went wrong");
         }
+    }
+    
+    @GetMapping("/trainers/{id}")
+    public ResponseEntity<Trainer> getById(@PathVariable int id) {
+    	return new ResponseEntity<>(ts.getbyId(id), HttpStatus.OK);
+    }
+    
+    @PutMapping("/trainers/{id}")
+    public ResponseEntity<Trainer> updateTrainer(@PathVariable int id, @RequestBody Object obj) {
+    	String[] money = obj.toString().split("=");
+    	int cash = Integer.parseInt(money[1].replace("}", ""));
+    	Trainer t = ts.getbyId(id);
+    	t.setMoney(cash);
+    	ts.saveTrainer(t);
+    	
+    	return new ResponseEntity<>(ts.saveTrainer(t), HttpStatus.OK);
     }
 }
